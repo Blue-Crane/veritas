20452d1ad2993386d1bae12e2a9bc912024fa8e4
melaveetha <retromeras@ya.ru>
2024-03-10 20:01:34 +0300

    Add [fetch] pages/home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,81 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class Home extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:json_annotation/json_annotation.dart';
+
+part 'home.g.dart';
+
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('Дом милый дом'));
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late Future<List<Product>> items;
+
+  @override
+  void initState() {
+    super.initState();
+    items = fetchItems();
   }
+
+  Future<List<Product>> fetchItems() async {
+    final response =
+        await http.get(Uri.parse('https://fakestoreapi.com/products'));
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body) as List<dynamic>;
+      return List.from(jsonData
+          .map((item) => Product.fromJson(item as Map<String, dynamic>)));
+    } else {
+      throw Exception("Network Failed");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+        child: FutureBuilder(
+          future: items,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final items =
+                  snapshot.data!.map((item) => Text(item.title)).toList();
+              return ListView(
+                children: items,
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            return const CircularProgressIndicator();
+          },
+        ));
+  }
+}
+
+@JsonSerializable()
+class Product {
+  final int id;
+  final String title;
+  final double price;
+  final String category;
+  final String description;
+  final String image;
+
+  const Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.category,
+    required this.description,
+    required this.image,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) =>
+      _$ProductFromJson(json);
 }
